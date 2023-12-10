69693a0fafaadad87a40ce45e32db5875a597b78
gabriel M <gabriel_jm_3@hotmail.com>
2023-12-09 23:49:26 -0300

    Specif

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,16 +2,14 @@ import HeroImage from '../src/assets/HERO-FINAL.jpg';
 
 function Hero() {
   return (
-    <div className="relative w-full h-screen bg-no-repeat bg-cover bg-fixed bg-center" 
-         style={{ backgroundImage: `url(${HeroImage})` }}>
+    <div className="hero" style={{ backgroundImage: `url(${HeroImage})` }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="flex justify-center items-center w-full h-full">
-        {/* Añade el logo centrado aquí */}
-        {/* <img id="logo" src={LogoImage} alt="Logo" className="max-w-xs md:max-w-sm lg:max-w-lg" /> */}
+      <div className="hero-text">
+        <h2>Tu Texto Aquí</h2>
+        {/* Otros elementos que desees centrar */}
       </div>
     </div>
   );
 }
-  
-  export default Hero;
-  
+
+export default Hero;
