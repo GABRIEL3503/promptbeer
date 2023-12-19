70bc6765362f49e555ebafede49f5aae1ebe08e7
gabriel M <gabriel_jm_3@hotmail.com>
2023-12-18 22:02:28 -0300

    css

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,9 +3,9 @@ import HeroImage from '../src/assets/HERO-FINAL.jpg';
 function Hero() {
   return (
     <div className="hero" style={{ backgroundImage: `url(${HeroImage})` }}>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-20"></div>
       <div className="hero-text">
-     <button>🦾 CHATEA CON NUETRRA IA</button>
+     <button>🦾 PREGUNTARLE AL CHATBOT</button>
         {/* Otros elementos que desees centrar */}
       </div>
     </div>
